07515c889343e0e2596042b7ae634cfc522f6fb2
gank <meatbagtv@gmail.com>
2022-11-06 15:51:34 -0500

    Updating collectEggs button

--- src/App.js
+++ src/App.js
@@ -183,7 +183,7 @@ const tabContent = [{
           <div className="flex justify-end px-4 pt-4">
 
           {loggedin === null && <button  onClick={async () => { await connect() }}className="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Connect</button>}
-          {loggedin !== null && <button  onClick={async () => { await collectEggs() }}className="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Verify</button>}
+          {loggedin !== null && <button  onClick={async () => { await collectEggs() }}className="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Switch</button>}
           </div>
           <div className="flex flex-col items-center pb-10">
             <img className="mb-3 w-24 h-24 rounded-full shadow-lg" src="https://www.meeplemountain.com/wp-content/uploads/2019/06/dimble.jpg" alt="goglim"/>
@@ -191,7 +191,7 @@ const tabContent = [{
             {Address !== null && <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white overflow-hidden">{truncateEthAddress(Address)}</h5>}
             <span className="text-sm text-gray-500 dark:text-gray-400">Gold Owned: <b>10</b></span>
             <div className="flex mt-4 space-x-3 md:mt-6">
-              <a href="#" onClick={async () => { await collectEggs() }} className="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Crack Egg</a>
+              <a href="#" onClick={async () => { await collectEggs() }} className="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Collect Goblin Eggs</a>
               
             </div>
           </div>
