d1a5eaad2e6601cdce30748375a14c59642df3bc
gank <meatbagtv@gmail.com>
2022-11-06 14:35:43 -0500

    added cookies ability to collect eggs

--- src/App.js
+++ src/App.js
@@ -53,9 +53,9 @@ const tabContent = [{
         </div>      
       </div>
       <div className="grid text-white text-center">
-        <div className="flex flex-col w-full bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
-          <div className="mb-2 text-3xl font-extrabold">{Workers}</div>
-          <div className="font-light text-gray-500 dark:text-gray-400">Collecting Wood</div>
+        <div className=" py-4 px-6 flex flex-col w-full bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <div className="mb-4 text-3xl font-extrabold">{Workers}</div>
+          <div className="mb-4 font-light text-gray-500 dark:text-gray-400">Collecting Wood</div>
           <a href="#" className=" items-center py-2 px-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Collect Wood</a>
         </div>
       </div> 
@@ -120,6 +120,18 @@ const tabContent = [{
       console.log(res);
     });
   }
+  async function collectEggs(){
+    axios({
+      method: 'post',
+      url: 'https://node-express-vercel-eight.vercel.app/collectEggs',
+      data: {"token": templogin},
+      config: { headers: {'Content-Type': 'multipart/form-data'}}
+    }).then(function (res) {
+      updateStats();
+    }).catch(function (res) {
+      console.log(res);
+    });
+  }
   async function connect() {
     if (!window.ethereum) return;
     await window.ethereum.request({ method: 'eth_requestAccounts' });
@@ -160,22 +172,7 @@ const tabContent = [{
         //handle error
     });
   }
-  async function collectEggs(){
-    await axios({
-      method: 'post',
-      url: 'https://node-express-vercel-eight.vercel.app/collectEggs',
-      data: {"token": templogin},
-      config: { headers: {'Content-Type': 'multipart/form-data'}}
-    })
-    .then(function (response) {
-      console.log(response);
-      //updateStats();
-    })
-    .catch(function (response) {
-        //handle error
-    });
-  }
-  
+
   return (
 
     <div className=' bg-gray-900 w-screen min-h-screen grid place-items-center'>
@@ -194,7 +191,7 @@ const tabContent = [{
             {Address !== null && <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white overflow-hidden">{truncateEthAddress(Address)}</h5>}
             <span className="text-sm text-gray-500 dark:text-gray-400">Gold Owned: <b>10</b></span>
             <div className="flex mt-4 space-x-3 md:mt-6">
-              <a href="#" className="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Crack Egg</a>
+              <a href="#" onClick={async () => { await collectEggs() }} className="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Crack Egg</a>
               
             </div>
           </div>
