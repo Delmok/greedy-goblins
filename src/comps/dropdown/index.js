f0b46398d864a70d7efa1d10eb9c30ecd7247a2f
gank <meatbagtv@gmail.com>
2022-11-07 22:48:15 -0500

    Changed some styling for the dropdown button

--- src/comps/dropdown/index.js
+++ src/comps/dropdown/index.js
@@ -43,20 +43,20 @@ export default function Example(props) {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className=" absolute right-0 z-10 mt-2 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none whitespace-nowrap">
           <div className="py-1">
             <Menu.Item>
               {({ active }) => (
-                <a href="#" onClick={()=>callWork(props.goblinid,"woodcutting")} className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700','block px-4 py-2 text-sm')}>
+                <button href="#" onClick={()=>callWork(props.goblinid,"woodcutting")} className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',' block px-4 py-2 text-sm w-full')}>
                   Collect Wood 
-                </a>
+                </button>
               )}
             </Menu.Item>
             <Menu.Item>
               {({ active }) => (
-                <a href="#" className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700','block px-4 py-2 text-sm')}>
+                <button href="#" className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700','block  px-4 py-2 text-sm w-full')}>
                   Kill Goblin
-                </a>
+                </button>
               )}
             </Menu.Item>
           </div>
