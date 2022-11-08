6ee5ea056cf86bcaf337f4f5dfc3b7fa3ab30d59
gank <meatbagtv@gmail.com>
2022-11-07 22:37:10 -0500

    Added comp for dropdown button giving options for each worker.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 import Tab from './comps/tab';
 import Gorkers from './comps/workers';
+import dropdownbutton from './comps/dropdown';
 import axios from 'axios';
 import truncateEthAddress from 'truncate-eth-address'
 import Cookies from 'universal-cookie';
--- src/comps/workers/index.js
+++ src/comps/workers/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
 import axios from 'axios';
 import Cookies from 'universal-cookie';
-
+import Dropdownbutton from '../dropdown';
 const cookies = new Cookies();
 
 
@@ -31,7 +31,7 @@ const Workers = (props) => {
 
     return (
         <>
-        <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+        <div className=" relative shadow-md sm:rounded-lg">
           <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
               <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                   <tr>
@@ -69,7 +69,7 @@ const Workers = (props) => {
                           {worker.power}
                           </td>
                           <td className="py-4 px-6">
-                              <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Kill</a>
+                            <Dropdownbutton goblinid = {worker.id}></Dropdownbutton>
                           </td>
                       </tr>
                       </tbody>
